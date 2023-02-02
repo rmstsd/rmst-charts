f59b407ee4e0ada21d3a8d62495b2faf28552635
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-02-02 12:56:37 +0800

    传统js 模块

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     react({
       // 取消HMR
       exclude: '**/*.tsx'
+    }),
+    legacy({
+      targets: ['defaults', 'not IE 11']
     })
   ]
 })
