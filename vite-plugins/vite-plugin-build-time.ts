d34006a176db6e584db87dc3e0480f3203b577f7
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-07-28 19:59:44 +0800

    timeZone Asia/Shanghai

--- vite-plugins/vite-plugin-build-time.ts
+++ vite-plugins/vite-plugin-build-time.ts
@@ -4,7 +4,7 @@ export default function buildTime(): Plugin {
   return {
     name: 'vite-plugin-build-time',
     transform(code, id) {
-      const dynamicValue = new Date().toLocaleString('zh-cn')
+      const dynamicValue = new Date().toLocaleString('en-US', { timeZone: 'Asia/Shanghai' })
       const updatedCode = code.replace('__Build_Time__', JSON.stringify(dynamicValue))
 
       return {
