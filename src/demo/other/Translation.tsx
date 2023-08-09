c36f1722a778db0f9e98a9f68ece0637c6510f46
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-08-09 10:38:51 +0800

    port

--- src/demo/other/Translation.tsx
+++ src/demo/other/Translation.tsx
@@ -24,28 +24,6 @@ const Translation = () => {
     <div>
       <button
         onClick={() => {
-          stageRef.current.ctx.clearRect(0, 0, 1000, 1000)
-          stageRef.current.ctx.transform(1, 0, 0, 1, 10, 10)
-
-          stageRef.current.renderStage()
-        }}
-      >
-        transform
-      </button>
-
-      <button
-        onClick={() => {
-          stageRef.current.ctx.clearRect(0, 0, 1000, 1000)
-          stageRef.current.ctx.setTransform(1, 0, 0, 1, 10, 10) // 会影响到为了解决模糊而设置的 ctx.scale(dpr, dpr)
-
-          stageRef.current.renderStage()
-        }}
-      >
-        setTransform
-      </button>
-
-      <button
-        onClick={() => {
           const rect = new Rect({
             x: 10,
             y: 10,
