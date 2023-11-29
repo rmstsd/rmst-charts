3fd6375d565dc81fc9e1a47e1780eb30ba54a13d
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-11-29 17:13:15 +0800

    zrender group drag test

--- src/main-router/router.zrender.tsx
+++ src/main-router/router.zrender.tsx
@@ -1,9 +1,10 @@
 import LayoutView from '@/components/LayoutView/LayoutView'
 
+import { IRouteObject } from './router'
+
 import QuickStart from '@/demo/3-zrender/QuickStart'
 import RectOver from '@/demo/3-zrender/RectOver'
-
-import { IRouteObject } from './router'
+import DragGroup from '@/demo/3-zrender/DragGroup'
 
 const rmstRenderRouteConfig: IRouteObject = {
   path: '/zrender',
@@ -11,7 +12,8 @@ const rmstRenderRouteConfig: IRouteObject = {
   uiConfig: { title: 'zrender', hidden: import.meta.env.PROD },
   children: [
     { path: 'QuickStart', element: <QuickStart /> },
-    { path: 'RectOver', element: <RectOver /> }
+    { path: 'RectOver', element: <RectOver /> },
+    { path: 'DragGroup', element: <DragGroup /> }
   ]
 }
 
