e4bf6df0200368273f2e9ac88ce262f13ada0167
rmst <rmstsd@qq.com>
2024-05-19 17:57:23 +0800

    bbox

--- packages/playground/src/main-router/6-other.tsx
+++ packages/playground/src/main-router/6-other.tsx
@@ -5,7 +5,7 @@ import AniCurve from '@/demo/6-other/贝塞尔曲线计算动画'
 import LinePath from '@/demo/6-other/过点曲线'
 import CurveRect from '@/demo/6-other/渐变'
 import Translation from '@/demo/6-other/Translation'
-import Brush from '@/demo/6-other/Brush'
+import BBox from '@/demo/6-other/BBox'
 import LineAnimate from '@/demo/6-other/折线计算动画'
 import 贝塞尔曲线峰值吸附 from '@/demo/6-other/贝塞尔曲线峰值吸附'
 import Rotate from '@/demo/6-other/Rotate'
@@ -28,7 +28,7 @@ const otherRouteConfig: IRouteObject = {
     { path: 'curveRect', element: <CurveRect />, uiConfig: { title: '渐变' } },
     { path: 'translation', element: <Translation /> },
     { path: 'rotate', element: <Rotate /> },
-    { path: 'brush', element: <Brush /> },
+    { path: 'brush', element: <BBox /> },
     { path: 'UpAniEnd', element: <UpAniEnd /> }
   ]
 }
