75dbdd90fd976cd04fa41b0c454bf0755663ba0a
rmstsd <rmstsd@qq.com>
2025-10-27 21:03:17 +0800

    up

--- packages/playground/src/demo/7-whiteboard/class/refLine.ts
+++ packages/playground/src/demo/7-whiteboard/class/refLine.ts
@@ -8,6 +8,8 @@ export class RefLine {
 
   refLine = { start: { x: 0, y: 0 }, end: { x: 0, y: 0 } }
 
+  refLines = [] as { start: ICoord; end: ICoord }[]
+
   getOffset(points: ICoord[], excludeGraphIds: string[]) {
     const refGraphs = this.wbEditor.graphLayer.children.filter(item => !excludeGraphIds.includes(item.id))
 
@@ -20,8 +22,6 @@ export class RefLine {
         x_map.set(element.x, [element.y])
       }
     }
-
-    console.log(x_map)
 
     const x_keys = [...x_map.keys()]
 
@@ -36,21 +36,34 @@ export class RefLine {
 
     const v_xks = [...vLineMap.keys()]
 
-    let closestXDist = Infinity
-    let realOffsetX = 0
-    let closestMinX_ans
+    // let closestXDist = Infinity
+    // let realOffsetX = 0
+    // let closestMinX_ans
 
-    for (const item of x_keys) {
+    const ddd = x_keys.map(item => {
       const closestMinX = getClosestVal(v_xks, item)
       const distMinX = Math.abs(closestMinX - item)
 
-      if (distMinX < closestXDist) {
-        closestXDist = distMinX
-
-        realOffsetX = closestMinX - item
-        closestMinX_ans = closestMinX
+      return {
+        distMinX,
+        realOffsetX: closestMinX - item,
+        closestMinX
       }
-    }
+    })
+
+    const closestXDist = Math.min(...ddd.map(item => item.distMinX))
+
+    // for (const item of x_keys) {
+    //   const closestMinX = getClosestVal(v_xks, item)
+    //   const distMinX = Math.abs(closestMinX - item)
+
+    //   if (distMinX < closestXDist) {
+    //     closestXDist = distMinX
+
+    //     realOffsetX = closestMinX - item
+    //     closestMinX_ans = closestMinX
+    //   }
+    // }
 
     const isEqualNum = (a: number, b: number) => Math.abs(a - b) < 0.00001
     const tol = 5 // 最小距离不能超过这个
@@ -58,27 +71,31 @@ export class RefLine {
     let offsetX
     // 确认偏移值 offsetX
     if (closestXDist <= tol) {
+      const realOffsetX = ddd.find(item => item.distMinX === closestXDist).realOffsetX
       offsetX = realOffsetX
-      // 这里考虑了一下浮点数误差
-      // if (isEqualNum(closestXDist, distMinX)) {
-      //   offsetX = closestMinX - minX
-      // } else if (isEqualNum(closestXDist, distMaxX)) {
-      //   offsetX = closestMaxX - maxX
-      // }
     }
 
     if (!isNil(offsetX)) {
-      const xs = points.filter(item => item.x + offsetX === closestMinX_ans).map(item => item.y)
-      const values = vLineMap.get(closestMinX_ans).concat(xs)
-      const minY = Math.min(...values)
-      const maxY = Math.max(...values)
+      const refLines = ddd
+        .filter(item => item.distMinX === closestXDist)
+        .map(dItem => {
+          const xs = points.filter(item => item.x + offsetX === dItem.closestMinX).map(item => item.y)
+          const values = vLineMap.get(dItem.closestMinX).concat(xs)
+          const minY = Math.min(...values)
+          const maxY = Math.max(...values)
 
-      this.refLine = {
-        start: this.wbEditor.coordSys.scene2World({ x: closestMinX_ans, y: minY }),
-        end: this.wbEditor.coordSys.scene2World({ x: closestMinX_ans, y: maxY })
-      }
+          return { start: { x: dItem.closestMinX, y: minY }, end: { x: dItem.closestMinX, y: maxY } }
+        })
+
+      const { coordSys } = this.wbEditor
+
+      this.refLines = refLines.map(item => ({ start: coordSys.scene2World(item.start), end: coordSys.scene2World(item.end) }))
+      //  {
+      //   start: this.wbEditor.coordSys.scene2World({ x: dItem.closestMinX, y: minY }),
+      //   end: this.wbEditor.coordSys.scene2World({ x: dItem.closestMinX, y: maxY })
+      // }
     } else {
-      this.refLine = null
+      this.refLines = []
     }
 
     return { x: offsetX, y: 0 }
@@ -88,15 +105,15 @@ export class RefLine {
     const { refLineLayer } = this.wbEditor
     refLineLayer.removeAllChildren()
 
-    if (this.refLine) {
+    this.refLines.forEach(item => {
       const line = new Line({
-        points: [this.refLine.start.x, this.refLine.start.y, this.refLine.end.x, this.refLine.end.y],
+        points: [item.start.x, item.start.y, item.end.x, item.end.y],
         strokeStyle: OpenColor.red[5],
         pointerEvents: 'none'
       })
 
       refLineLayer.append(line)
-    }
+    })
   }
 }
 
