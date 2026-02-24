35257ce778a6f4cd1829f33f5128b2d8366b8a6d
rmstsd <rmstsd@qq.com>
2026-02-24 21:12:44 +0800

    圆形 handle

--- packages/playground/src/demo/7-whiteboard/class/controlHandleManager.ts
+++ packages/playground/src/demo/7-whiteboard/class/controlHandleManager.ts
@@ -2,6 +2,7 @@ import WhiteboardEditor from '../whiteboardEditor'
 import { ToolEnum, ToolEnumKey } from './ToolManager/constant'
 import { ICustomHandleController } from './ToolManager/type'
 import { RectController } from './ToolManager/ToolSelect/ToolHandle/RectController'
+import { EllipseController } from './ToolManager/ToolSelect/ToolHandle/EllipseController'
 
 export default class ControlHandleManager {
   private controllers: Map<ToolEnumKey, ICustomHandleController> = new Map()
@@ -12,6 +13,7 @@ export default class ControlHandleManager {
 
   private registerControllers() {
     this.controllers.set(ToolEnum.Rect as ToolEnumKey, new RectController())
+    this.controllers.set(ToolEnum.Ellipse as ToolEnumKey, new EllipseController())
   }
 
   getController(wbType: ToolEnumKey): ICustomHandleController | undefined {
